1f3a85450fccbcc982adc1f4818490549f7d3f53
Shannon Lal <shannonlal@hotmail.com>
2018-11-21 21:34:20 -0500

    First attempt at json schema

--- src/common/constants.js
+++ src/common/constants.js
@@ -18,6 +18,42 @@ const DIALECTS = {
     BIGQUERY: 'bigquery'
 };
 
+const SQL_ATTRIBUTES_SCHEMA = {
+    type: 'object',
+
+    properties:{
+        username:{
+            type:'string',
+            label: 'Username',
+            errorMessage: 'Username is required'
+        },
+        password:{
+            type:'string',
+            label: 'Username',
+            errorMessage: 'Password is required'
+        },
+        host:{
+            type:'string',
+            label: 'Host',
+            errorMessage: 'Host is required'
+        },
+        port:{
+            type:'number',
+            label: 'Port',
+            errorMessage: 'Port is required',
+            description: 'Server port number (e.g. 3306)'
+        },
+        database:{
+            type:'string',
+            label: 'Database',
+            errorMessage: 'Database is required'
+        }
+    },
+    required:['username','password','host','port','database']
+    
+};
+
 module.exports = {
-    DIALECTS
+    DIALECTS,
+    SQL_ATTRIBUTES_SCHEMA
 };
