318ba15d61d1c10c7411c4e09ee8daed63c3726b
Shannon Lal <shannonlal@hotmail.com>
2018-12-04 19:52:31 -0500

    Comment in connection label

--- examples/client/src/App.js
+++ examples/client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import DBSelector from './components/DBSelector';
 import {getDialects} from './common/constants';
 import ResultsTable from './components/ResultsTable';
+import ConnectionDialog from './components/ConnectionDialog';
 
 
 class App extends Component {
@@ -11,18 +12,20 @@ class App extends Component {
     super(props);
     this.state = {
       rows:[],
-      columns:[]
+      columns:[],
+      dialect:''
     };
   }
 
   connectorSelected( dialect ){
     console.log( 'Dialect Selected', dialect.value);
+    this.setState({dialect:dialect.value});
   }
   render() {
     return (
       <div className="App">
         <DBSelector  options={getDialects()} connectorSelected={this.connectorSelected.bind(this)}/>
-
+        <ConnectionDialog dialect={this.state.dialect} />
         <ResultsTable columnNames={this.state.columns} rows={this.state.rows} />
       </div>
     );
