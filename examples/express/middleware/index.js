abfce9f45f2a2627c6cbec4b88445276e725fbd2
Shannon Lal <shannonlal@hotmail.com>
2018-12-07 14:09:39 -0500

    Moving examples to seperate repo

--- examples/express/middleware/index.js
+++
@@ -1,7 +0,0 @@
-const {validateRequest} = require('./validator');
-const loggerMiddleware = require('./logger');
-
-module.exports = {
-    validateRequest,
-    loggerMiddleware
-};
--- examples/express/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = require('winston');
-
-const loggerMiddleware = function(req, res, next) {
-    logger.info(`Executing ${req.method} on path ${req.url}`);
-    next();
-};
-
-module.exports = loggerMiddleware;
