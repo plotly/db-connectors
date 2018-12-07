bbfdf1a96802ebbb5fe9d8c8b7cef7230e081bfb
Shannon Lal <shannonlal@hotmail.com>
2018-12-07 14:38:30 -0500

    Check in Hadoop version

--- test/schema/schema.hadoop.spec.js
+++ test/schema/schema.hadoop.spec.js
@@ -15,7 +15,7 @@ describe('Hadopp Schema:', function () {
             port: 9000,
             database:'plotly.db',
             timeout: 1000
-        }
+        };
         let rst = v.validate( hadoopConnection, SCHEMAS.HADOOP_ATTRIBUTES_SCHEMA);
         assert(rst.valid);
     });
@@ -25,7 +25,7 @@ describe('Hadopp Schema:', function () {
             port: 9000,
             database:'plotly.db',
             timeout: 1000
-        }
+        };
 
         let rst;
         try{
@@ -34,5 +34,48 @@ describe('Hadopp Schema:', function () {
         }catch(err){
             assert.isOk(!rst.valid, 'Test Passed failed validation');
         }
+    });
+
+    it('should fail to validate hadoop connection because missing port', function() {
+        const hadoopConnection = {
+            host: '127.0.0.1',
+            database:'plotly.db',
+            timeout: 1000
+        };
+
+        let rst;
+        try{
+            rst = v.validate( hadoopConnection, SCHEMAS.HADOOP_ATTRIBUTES_SCHEMA);
+            expect(rst.errors[0].message).to.equal('requires property "port"');
+        }catch(err){
+            assert.isOk(!rst.valid, 'Test Passed failed validation');
+        }
+    });
+
+    it('should fail to validate hadoop connection because missing timeout', function() {
+        const hadoopConnection = {
+            host: '127.0.0.1',
+            port: 9000,
+            database:'plotly.db',
+        };
+
+        let rst;
+        try{
+            rst = v.validate( hadoopConnection, SCHEMAS.HADOOP_ATTRIBUTES_SCHEMA);
+            expect(rst.errors[0].message).to.equal('requires property "timeout"');
+        }catch(err){
+            assert.isOk(!rst.valid, 'Test Passed failed validation');
+        }
+    });
+
+    it('should validate hadoop connection without database', function() {
+        const hadoopConnection = {
+            host: '127.0.0.1',
+            port: 9000,
+            timeout: 1000
+        };
+
+        let rst = v.validate( hadoopConnection, SCHEMAS.HADOOP_ATTRIBUTES_SCHEMA);
+        assert(rst.valid);
     });
 });
